a6c64eb1cfdeb1e6e0f1a83f8670051c21a38d45
Patrick Perey <the4dpatrick@yahoo.com>
2014-03-16 00:08:22 -0700

    Finish 037

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,11 @@
 class Project < ActiveRecord::Base
+  validates_presence_of :name
+
+  def self.search(search)
+    if search
+      where("name LIKE ?", "%#{search}%")
+    else
+      all
+    end
+  end
 end
